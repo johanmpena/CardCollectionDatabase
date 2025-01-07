509d89c277d1a8e2fe92e11c0d5d740004ded88d
Johan <jpena@perscholas.org>
2025-01-07 10:08:43 -0500

    Trying to get the home controller to work.

--- demo/src/main/java/com/CardCollectionDatabase/demo/controller/HomeController.java
+++ demo/src/main/java/com/CardCollectionDatabase/demo/controller/HomeController.java
@@ -1,9 +1,10 @@
-package com.CardCollectionDatabase.demo.controllers;
+package com.CardCollectionDatabase.demo.controller;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Component
+@Controller
 public class HomeController {
 
     @GetMapping("/")
